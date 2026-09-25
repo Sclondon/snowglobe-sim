e6cf5b15b6585b6c00ab2fc1a2cb4606e879de6a
Sclondon <stewartlondon21@gmail.com>
2026-09-24 19:35:53 -0600

    Deploy web build from 14e9dce

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790296250|2306901';
+const CACHE_VERSION = '1790300150|1995075';
 /** @type {string} */
 const CACHE_PREFIX = 'SnowglobeSimulat-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
